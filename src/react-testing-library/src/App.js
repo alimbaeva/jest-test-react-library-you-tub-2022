e42819b7d9455240c7fa36c11664fccc0c787d6d
Asel <asel.alimbaeva.kk@gmail.com>
2022-10-03 07:50:57 +0300

    feat: add test app file, userEvent, fireEvent

--- src/react-testing-library/src/App.js
+++ src/react-testing-library/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+  const [toggle, setToggle] = useState(false);
+  const [value, setValue] = useState('');
+
+  const onClick = () => setToggle(prev => !prev);
+  useEffect(() => {
+    setTimeout(() => {
+      setData({});
+    }, 100);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 data-testid='value-el'>{value}</h1>
+       {toggle && <div data-testid='toggle-el'>toggle</div>}
+       {data && <div style={{color: 'red'}}>data</div>}
+       <h1>Hello world</h1>
+       <button data-testid='toggle-btn' onClick={onClick}>click me</button>
+       <input onChange={e => setValue(e.target.value)} type='text' placeholder='input value...' />
     </div>
   );
 }
--- src/react-testing-library/src/App.test.js
+++ src/react-testing-library/src/App.test.js
@@ -1,8 +1,110 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+//fireEvent - это искуственное событие
+//userEvent - близко к пользователю, обрабатывается события нажатия клавиши и тд
+
+describe("Test App", () => {
+  test('renders learn react link', () => {
+    render(<App />);
+  
+    //https://testing-library.com/docs/queries/bytext
+    const HelloWorld = screen.getByText(/Hello world/i);
+  
+    //https://testing-library.com/docs/queries/byrole/
+    const btn = screen.getByRole('button');
+    
+    //https://testing-library.com/docs/queries/byplaceholdertext/
+    const input = screen.getByPlaceholderText(/input value.../i);
+  
+    //https://testing-library.com/docs/guide-disappearance/#nottobeinthedocument
+    expect(HelloWorld).toBeInTheDocument();
+    expect(btn).toBeInTheDocument();
+    expect(input).toBeInTheDocument();
+  
+    //https://jestjs.io/docs/expect#tomatchsnapshotpropertymatchers-hint
+    expect(input).toMatchSnapshot();
+    screen.debug();
+  });
+
+  test('renders learn react link query', () => {
+    render(<App />);
+    
+    //https://testing-library.com/docs/dom-testing-library/cheatsheet/#queries
+    const hellooWorldEl = screen.queryByText(/hellow2/i);
+    
+    //https://jestjs.io/docs/expect#tobenull
+    expect(hellooWorldEl).toBeNull();
+  });
+
+  test('renders learn react link finde', async () => {
+    render(<App />);
+    screen.debug();
+
+    //https://testing-library.com/docs/example-findbytext/
+    const hellooWorldEl = await screen.findByText(/data/i);
+    
+    //https://testing-library.com/docs/guide-disappearance/#nottobeinthedocument
+    expect(hellooWorldEl).toBeInTheDocument();
+    screen.debug();
+  });
+
+  test('renders learn react link Style', async () => {
+    render(<App />);
+
+    //https://testing-library.com/docs/example-findbytext/
+    const hellooWorldEl = await screen.findByText(/data/i);
+    
+    expect(hellooWorldEl).toHaveStyle({color: 'red'});
+  });
+
+  test('Click Event fireEvent', async () => {
+    render(<App />);
+
+    //https://testing-library.com/docs/queries/bytestid/
+    const btn = screen.getByTestId('toggle-btn');
+
+    //https://testing-library.com/docs/dom-testing-library/cheatsheet/#queries
+    expect(screen.queryByTestId('toggle-el')).toBeNull();
+
+    //https://testing-library.com/docs/vue-testing-library/cheatsheet/#firing-events
+    fireEvent.click(btn);
+    expect(screen.queryByTestId('toggle-el')).toBeInTheDocument();
+    fireEvent.click(btn);
+    expect(screen.queryByTestId('toggle-el')).toBeNull();
+    
+  });
+
+  test('Click Input fireEvent', async () => {
+    render(<App />);
+
+    //https://testing-library.com/docs/queries/byplaceholdertext/
+    const input = screen.getByPlaceholderText(/input value.../i);
+
+    //https://dev.to/jpblancodb/testing-svelte-components-with-jest-53h3
+    expect(screen.queryByTestId('value-el')).toContainHTML('');
+
+    //https://testing-library.com/docs/vue-testing-library/cheatsheet/#firing-events
+    fireEvent.input(input, {
+      target: {value: '12345'}
+    });
+    expect(screen.queryByTestId('value-el')).toContainHTML('12345');
+    
+  });
+
+  test('Click Input userEvent', async () => {
+    render(<App />);
+
+    //https://testing-library.com/docs/queries/byplaceholdertext/
+    const input = screen.getByPlaceholderText(/input value.../i);
+
+    //https://dev.to/jpblancodb/testing-svelte-components-with-jest-53h3
+    expect(screen.queryByTestId('value-el')).toContainHTML('');
+
+    //https://testing-library.com/docs/ecosystem-user-event/#keyboardtext-options
+    userEvent.type(input, '09876')
+    expect(screen.queryByTestId('value-el')).toContainHTML('09876');
+    
+  });
+})
